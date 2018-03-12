48b212ef54964ad5003c898cec66222e8d209806
胡祥文 <huxiangwen@che001.com>
2018-03-12 14:41:16 +0800

    修改typeChecker， 参考lodash

--- src/utils/typeChecker.js
+++ src/utils/typeChecker.js
@@ -1,10 +1,54 @@
-const checker = {};
+const toString = Object.prototype.toString,
+    checker = {}
 
-['Function', 'String', 'Number', 'Boolean', 'Object', 'Undefined'].map(function (v) {
-    const vlower = v.toLowerCase()
-    this[`is${v}`] = function (val) {
-        return typeof val === vlower
+
+function getTag(v) {
+    return toString.call(v)
+}
+
+function isObjectLike(v) {
+    return typeof v == 'object' && v !== null
+}
+
+function isLength(v) {
+    return typeof v == 'number' &&
+        v > -1 && v % 1 == 0 && v <= Number.MAX_SAFE_INTEGER
+}
+
+['Function', 'String', 'Number', 'Boolean', 'Object', 'Symbol'].map(function (t) {
+    const vlower = t.toLowerCase()
+    this[`is${t}`] = function (v) {
+        return typeof v === vlower || isObjectLike(v) && getTag(v) === `[object ${t}]`
+    }
+}, checker);
+
+['RegExp', 'Set', 'WeakSet', 'Map', 'WeakMap', 'Error', 'ArrayBuffer', 'Arguments'].map(function (t) {
+    this[`is${t}`] = function (v) {
+        return isObjectLike(v) && getTag(v) === `[object ${t}]`
     }
 }, checker)
 
-module.exports = checker
+function isArray(v) {
+    return Array.isArray(v)
+}
+
+function isArrayLike(v) {
+    return isObjectLike(v) && isLength(v.length)
+}
+
+function isNull(v) {
+    return v === null
+}
+
+function isUndefined(v) {
+    return v === undefined
+}
+
+module.exports = Object.assign(checker, {
+    isLength,
+    isArray,
+    isArrayLike,
+    isObjectLike,
+    isNull,
+    isUndefined
+})
